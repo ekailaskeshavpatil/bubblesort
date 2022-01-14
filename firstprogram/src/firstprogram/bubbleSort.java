f144b399c144f406675119bbe32b20935803dda6
kailas.patil <ekailaskeshavpatil@gmail.com>
2022-01-14 20:01:16 +0530

    initial commit

---
+++ firstprogram/src/firstprogram/bubbleSort.java
@@ -0,0 +1,28 @@
+package firstprogram;
+
+public class bubbleSort {
+
+	static void bubbleSortMethod(int[] a) {
+		int length = a.length;
+
+		for (int i = 0; i < length - 1; i++) {
+			for (int j = 1; j < length - i; j++) {
+
+				if (a[j - 1] > a[j]) {
+					int temp = a[j - 1];
+					a[j - 1] = a[j];
+					a[j] = temp;
+				}
+			}
+
+		}
+	}
+
+	public static void main(String[] args) {
+		int a[] = { 12, 79, 55, 29, 111 };
+		bubbleSortMethod(a);
+		for (int i = 0; i < a.length; i++) {
+			System.out.print(a[i] + " ");
+		}
+	}
+}
